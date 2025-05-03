8ea66a335333d499f5297791f2022eb533684180
Lyndon Simango <>
2025-05-03 22:32:14 +0200

    Corrected  catergorical data grouping them appropriately, Viewed duplicate rows

--- DataQualityAssurance.sql
+++ DataQualityAssurance.sql
@@ -58,23 +58,90 @@ WHERE a.propertyaddress IS NULL;
 -- I will separate the address in parts. Address, Town etc.
 -- I will use ',' as the character to separate the two and TRIM off the spaces off the fields
 SELECT 
-    RTRIM(SUBSTR(propertyaddress, 1, INSTR(propertyaddress, ',') - 1)) AS AddressSplit,
-    LTRIM(SUBSTR(propertyaddress, INSTR(propertyaddress, ',') + 1)) AS TownSplit
+    TRIM(SUBSTR(propertyaddress, 1, INSTR(propertyaddress, ',') - 1)) AS AddressSplit,
+    TRIM(SUBSTR(propertyaddress, INSTR(propertyaddress, ',') + 1)) AS TownSplit
 FROM PropertyData;
 
 -- Creating the fields to house the new data.
 -- Using ALTER and the UPDATE statement.
 ALTER TABLE propertydata
 ADD AddressSplit VARCHAR2(35);
-
 UPDATE propertydata
-SET AddressSplit = RTRIM(SUBSTR(propertyaddress, 1, INSTR(propertyaddress, ',') - 1))
+SET AddressSplit = TRIM(SUBSTR(propertyaddress, 1, INSTR(propertyaddress, ',') - 1));
 
 ALTER TABLE propertydata
 ADD TownSplit VARCHAR2(35);
-
 UPDATE propertydata
-SET TownSplit = LTRIM(SUBSTR(propertyaddress, INSTR(propertyaddress, ',') + 1))
+SET TownSplit = TRIM(SUBSTR(propertyaddress, INSTR(propertyaddress, ',') + 1));
+
+-- Now to do the same for the Owner Address using the RegEx fuctions
+SELECT 
+    REGEXP_SUBSTR(owneraddress, '[^,]+', 1, 1) AS ownerAddressSplit,
+    REGEXP_SUBSTR(owneraddress, '[^,]+', 1, 2) AS ownerTownSplit,
+    REGEXP_SUBSTR(owneraddress, '[^,]+', 1, 3) AS ownerStateSplit
+FROM PropertyData;
+
+--Now creating new columns for the data.
+ALTER TABLE propertydata
+ADD ownerAddressSplit VARCHAR2(35);
+UPDATE propertydata
+SET ownerAddressSplit = TRIM(REGEXP_SUBSTR(owneraddress, '[^,]+', 1, 1));
+
+ALTER TABLE propertydata
+ADD ownerTownSplit VARCHAR2(35);
+UPDATE propertydata
+SET ownerTownSplit = TRIM(REGEXP_SUBSTR(owneraddress, '[^,]+', 1, 2));
+
+ALTER TABLE propertydata
+ADD ownerStateSplit VARCHAR2(35);
+UPDATE propertydata
+SET ownerStateSplit = TRIM(REGEXP_SUBSTR(owneraddress, '[^,]+', 1, 3));
+
+-- Checking correctness of the catergorical values.
+-- LANDUSE field has unique and distict data, however SOLDASVACANT has
+-- inconsistant data that needs correcting
+SELECT DISTINCT(landuse)
+FROM propertydata;
+
+SELECT DISTINCT(soldasvacant), count(soldasvacant)
+FROM propertydata
+GROUP BY soldasvacant
+ORDER BY 2 DESC;
+
+-- Now to correct the categorical data using the CASE statement
+SELECT soldasvacant,
+CASE 
+    WHEN soldasvacant ='Y' THEN 'Yes'
+    WHEN soldasvacant = 'N' THEN 'No'
+    ELSE soldasvacant
+END AS correctedSoldAsVacant
+FROM propertydata;
+
+-- Now to update the corrected values
+UPDATE propertydata
+SET soldasvacant = (
+CASE 
+    WHEN soldasvacant ='Y' THEN 'Yes'
+    WHEN soldasvacant = 'N' THEN 'No'
+    ELSE soldasvacant
+END)
+
+
+-- Dealing with duplicates (Not good practice to delete data in a database)
+-- Removed UNIQUEID since the could be unique on duplicates records
+SELECT PARCELID, LANDUSE, PROPERTYADDRESS, SALEDATE, SALEPRICE,
+LEGALREFERENCE, SOLDASVACANT, OWNERNAME, OWNERADDRESS, ACREAGE, TAXDISTRICT,
+LANDVALUE, BUILDINGVALUE, TOTALVALUE, YEARBUILT, BEDROOMS, FULLBATH, HALFBATH,
+COUNT(*) AS duplicate_count
+FROM propertydata
+GROUP BY PARCELID, LANDUSE, PROPERTYADDRESS, SALEDATE, SALEPRICE,
+LEGALREFERENCE, SOLDASVACANT, OWNERNAME, OWNERADDRESS, ACREAGE, TAXDISTRICT,
+LANDVALUE, BUILDINGVALUE, TOTALVALUE, YEARBUILT, BEDROOMS,FULLBATH, HALFBATH
+HAVING COUNT(*) > 1;
+
+
+
+--select * From propertydata
 
 
 
